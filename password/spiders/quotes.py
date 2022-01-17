003b95d41a35eb36ca624dbf83d7e92dda7acb64
FrancoB4 <francobonfigliovazquez@gmail.com>
2022-01-17 12:27:42 -0300

    Comments

--- password/spiders/quotes.py
+++ password/spiders/quotes.py
@@ -1,12 +1,14 @@
 import scrapy
 import lxml.html as html
 from scrapy import FormRequest
+# Descargamos el item que hems creado en ../items.py
 from ..items import QuoteTutorialItem
 
 
 TOKEN = 'body//input[@name="csrf_token"]/@value'
 
 class QuotesScraper(scrapy.Spider):
+    # Definimos en nombre del Spider y el link de origen
     name = 'quotes'
     start_urls = [
         'https://quotes.toscrape.com/login'
@@ -15,6 +17,7 @@ class QuotesScraper(scrapy.Spider):
 
     def parse(self, response):
         token = response.xpath(TOKEN).get()
+        # Aqui se va aproducir el login, debemos ver en la pagina cuales son las variables necesarias para acceder (en network del inspector bajo el nombre user-login)
         return FormRequest.from_response(response, 
         formdata={
             'csrf_token': token,
